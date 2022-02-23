b71faa14b990e4a17aa05948a6b170171d035041
John Vincent <git@tenseventyseven.cf>
2022-02-23 14:02:16 +0800

    input: Set device info messages as 'debug-only'
    Melfas drivers frequently abuse this just to show features being enabled. Since we are certain of the driver's stability at this point, just make input_info messages debug-only as well.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- include/uapi/linux/input.h
+++ include/uapi/linux/input.h
@@ -47,7 +47,7 @@
 	({										\
 		static char input_log_buf[INPUT_LOG_BUF_SIZE];				\
 		snprintf(input_log_buf, sizeof(input_log_buf), "%s %s", SECLOG, fmt);	\
-		dev_info(dev, input_log_buf, ## __VA_ARGS__);				\
+		dev_dbg(dev, input_log_buf, ## __VA_ARGS__);				\
 		if (mode) {								\
 			if (dev)							\
 				snprintf(input_log_buf, sizeof(input_log_buf), "%s %s",	\
@@ -75,7 +75,7 @@
 ({										\
 	static char input_log_buf[INPUT_LOG_BUF_SIZE];				\
 	snprintf(input_log_buf, sizeof(input_log_buf), "%s %s", SECLOG, fmt);	\
-	dev_info(dev, input_log_buf, ## __VA_ARGS__);				\
+	dev_dbg(dev, input_log_buf, ## __VA_ARGS__);				\
 	if (mode) {								\
 		if (dev)							\
 			snprintf(input_log_buf, sizeof(input_log_buf), "%s %s", \
@@ -99,7 +99,7 @@
 	({										\
 		static char input_log_buf[INPUT_LOG_BUF_SIZE];				\
 		snprintf(input_log_buf, sizeof(input_log_buf), "%s %s", SECLOG, fmt);	\
-		dev_info(dev, input_log_buf, ## __VA_ARGS__);				\
+		dev_dbg(dev, input_log_buf, ## __VA_ARGS__);				\
 	 })
 #define input_err(mode, dev, fmt, ...)						\
 	({										\
