b82e565a38b78e263e45a10b9ee1958c6584e5ec
John Vincent <git@tenseventyseven.cf>
2022-07-10 18:28:40 +0800

    sched: ems: ontime: Improve 'ontime' CPU selection
    Ontime tasks are both performance and latency-sensitive. Ensure that we place these tasks on CPUs with minimum utilization as much as possible.
    
    For idle CPUs, ensure that we both get the CPU with the shallowest idle state (for fast wake and less power consumption) as well as the lowest utilization. Otherwise, use the estimated utilization of the task instead of its exact utilization so we have allowance for migration.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/sched/ems/ontime.c
+++ kernel/sched/ems/ontime.c
@@ -189,6 +189,7 @@ ontime_select_target_cpu(struct task_struct *p, struct cpumask *fit_cpus)
 		int i;
 		int best_cpu = -1, backup_cpu = -1;
 		unsigned int min_exit_latency = UINT_MAX;
+		unsigned long min_idle_util = ULONG_MAX;
 		unsigned long min_util = ULONG_MAX;
 		unsigned long coverage_util;
 
@@ -198,6 +199,7 @@ ontime_select_target_cpu(struct task_struct *p, struct cpumask *fit_cpus)
 		coverage_util = capacity_orig_of(cpu) * get_coverage_ratio(cpu);
 
 		for_each_cpu_and(i, cpu_coregroup_mask(cpu), cpu_active_mask) {
+			unsigned long wake_util = cpu_util_wake(i, p);
 
 			if (!cpumask_test_cpu(i, tsk_cpus_allowed(p)))
 				continue;
@@ -214,13 +216,20 @@ ontime_select_target_cpu(struct task_struct *p, struct cpumask *fit_cpus)
 					break;
 				}
 
-				if (idle->exit_latency < min_exit_latency) {
-					min_exit_latency = idle->exit_latency;
-					best_cpu = i;
-				}
+				if (idle->exit_latency > min_exit_latency)
+					continue;
+
+				/* if same cstate, select lower util */
+				if (idle->exit_latency == min_exit_latency &&
+					wake_util >= min_idle_util)
+					continue;
+
+				min_exit_latency = idle->exit_latency;
+				min_idle_util = wake_util;
+				best_cpu = i;
 			} else {
 				/* 2. Find cpu that have to spare */
-				unsigned long new_util = task_util(p) + cpu_util_wake(i, p);
+				unsigned long new_util = wake_util + task_util_est(p);
 
 				if (new_util * 100 >= coverage_util)
 					continue;
