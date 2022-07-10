0f8c394ee28bbf6706916707bb82f9a1bceb9fd7
John Vincent <git@tenseventyseven.cf>
2022-07-10 20:02:02 +0800

    sched: tune: Extend schedtune_ux_task() to cover the 'prefer-perf' hint
    Exynos Mobile Scheduler (EMS) uses the 'prefer-perf' hint to place tasks onto high-performance CPUs, as this typically means that the task is performance and/or latency-sensitive. Typically on CFS, only the 'schedtune.boost' hint is used for this.
    
    We have changed EMS' 'prefer-idle' behavior to prefer lower capacity CPUs as much as possible to maintain good battery life and the UX task seems to get placed in this logic from time-to-time, making its performance not so great.
    
    Let's extend schedtune_ux_task() for Exynos' 'prefer-perf' hint as well so we ensure that the task currently in the UX are on the big cluster cores as much as possible to maintain optimal performance without bringing miscellaneous tasks with it.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -438,7 +438,7 @@ int schedtune_cpu_boost(int cpu)
 	return bg->boost_max;
 }
 
-static inline int schedtune_adj_ta(struct task_struct *p)
+static inline int schedtune_ux_task(struct task_struct *p, int orig)
 {
 	struct schedtune *st;
 	char name_buf[NAME_MAX + 1];
@@ -446,11 +446,11 @@ static inline int schedtune_adj_ta(struct task_struct *p)
 
 	/* We only care about adj == 0 */
 	if (adj != 0)
-		return 0;
+		return orig;
 
 	/* Don't touch kthreads */
 	if (p->flags & PF_KTHREAD)
-		return 0;
+		return orig;
 
 	st = task_schedtune(p);
 	cgroup_name(st->css.cgroup, name_buf, sizeof(name_buf));
@@ -459,7 +459,7 @@ static inline int schedtune_adj_ta(struct task_struct *p)
 		return 1;
 	}
 
-	return 0;
+	return orig;
 }
 
 int schedtune_task_boost(struct task_struct *p)
@@ -473,7 +473,7 @@ int schedtune_task_boost(struct task_struct *p)
 	/* Get task boost value */
 	rcu_read_lock();
 	st = task_schedtune(p);
-	task_boost = max(st->boost, schedtune_adj_ta(p));
+	task_boost = schedtune_ux_task(p, st->boost);
 	rcu_read_unlock();
 
 	return task_boost;
@@ -542,7 +542,7 @@ int schedtune_prefer_perf(struct task_struct *p)
 	/* Get prefer_perf value */
 	rcu_read_lock();
 	st = task_schedtune(p);
-	prefer_perf = max(st->prefer_perf, kpp_status(st->idx));
+	prefer_perf = max(schedtune_ux_task(p, st->prefer_perf), kpp_status(st->idx));
 	rcu_read_unlock();
 
 	return prefer_perf;
